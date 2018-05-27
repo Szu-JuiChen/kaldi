94977000ad8974925b24b28e119885ffafc6edf6
Chen Szu-Jui <cszu@b03.clsp.jhu.edu>
2018-05-26 21:05:38 -0400

    WIP

--- egs/chime5/s5/local/nnet3/run_ivector_common.sh
+++ egs/chime5/s5/local/nnet3/run_ivector_common.sh
@@ -136,21 +136,21 @@ if [ $stage -le 6 ]; then
   # that's the data we extract the ivectors from, as it's still going to be
   # valid for the non-'max2' data, the utterance list is the same.
 
-  # ivectordir=exp/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires
-  # if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $ivectordir/storage ]; then
-    # utils/create_split_dir.pl /export/b0{5,6,7,8}/$USER/kaldi-data/ivectors/chime5-$(date +'%m_%d_%H_%M')/s5/$ivectordir/storage $ivectordir/storage
-  # fi
+  ivectordir=exp/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires
+  if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $ivectordir/storage ]; then
+    utils/create_split_dir.pl /export/b0{5,6,7,8}/$USER/kaldi-data/ivectors/chime5-$(date +'%m_%d_%H_%M')/s5/$ivectordir/storage $ivectordir/storage
+  fi
 
 
   # having a larger number of speakers is helpful for generalization, and to
   # handle per-utterance decoding well (iVector starts at zero).
-  # temp_data_root=${ivectordir}
-  # utils/data/modify_speaker_info.sh --utts-per-spk-max 2 \
-    # data/${train_set}_sp_hires ${temp_data_root}/${train_set}_sp_hires_max2
+  temp_data_root=${ivectordir}
+  utils/data/modify_speaker_info.sh --utts-per-spk-max 2 \
+    data/${train_set}_sp_hires ${temp_data_root}/${train_set}_sp_hires_max2
 
-  # steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj ${nj} \
-    # ${temp_data_root}/${train_set}_sp_hires_max2 \
-    # exp/nnet3${nnet3_affix}/extractor $ivectordir
+  steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj ${nj} \
+    ${temp_data_root}/${train_set}_sp_hires_max2 \
+    exp/nnet3${nnet3_affix}/extractor $ivectordir
 
   # Also extract iVectors for the test data, but in this case we don't need the speed
   # perturbation (sp).
